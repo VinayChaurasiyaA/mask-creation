6b464a9ee896144d75a03570598339cdef1751d4
vinay <vinaychaurasiya73@gmail.com>
2023-07-31 23:35:54 +0530

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <main className="main">
+      <div className="mask">
+        <p>
+          A visual disgner - with skills which are not yet replaced by A.I.
+          (yet)- making good shit only because paycheck is pretty well.
+        </p>
+      </div>
+
+      <div className="body">
+        <p>
+          I'm a <span>selectively skilled </span>
+          product developer with strong focus on producing high quality &
+          impactful experience
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
