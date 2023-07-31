303f1fda51c22395b6e6f6eebc45d9d2808a0cec
vinay <vinaychaurasiya73@gmail.com>
2023-08-01 00:10:58 +0530

    nice-one

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
-
+import { motion } from "framer-motion";
+import useMousePosition from "./utils/useMousePosition";
 import "./App.scss";
 
 function App() {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
   return (
     <main className="main">
-      <div className="mask">
-        <p>
+      <motion.div
+        className="mask"
+        animate={{
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+      >
+        <p
+          onMouseEnter={() => {
+            setIsHovered(true);
+          }}
+          onMouseLeave={() => {
+            setIsHovered(false);
+          }}
+        >
           A visual disgner - with skills which are not yet replaced by A.I.
           (yet)- making good shit only because paycheck is pretty well.
         </p>
-      </div>
+      </motion.div>
 
       <div className="body">
         <p>
